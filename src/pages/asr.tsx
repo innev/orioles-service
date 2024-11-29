385c56c5398b0fe5e652d65123b24f4c5a2b2ad4
sunjunzhao <zhaozhao200295@gmail.com>
2024-11-30 01:31:57 +0800

    增加部署配置

--- src/pages/asr.tsx
+++ src/pages/asr.tsx
@@ -1,4 +1,4 @@
-import { RecordIcon } from '@/components/icons';
+import RecordIcon from '@/components/icons/Record';
 import HomeLayout from '@/components/layouts/HomeLayout';
 import useMediaRecorder from '@/hooks/useMediaRecorder';
 import { audioReader, transcriptions } from '@/utils';
