d3f76a67352485a49ef15ae115385add82200993
sunjunzhao <zhaozhao200295@gmail.com>
2024-12-14 00:21:23 +0800

    优化API请求

--- src/app/(apps)/vip/page.tsx
+++ src/app/(apps)/vip/page.tsx
@@ -1,14 +1,15 @@
+import AppNav from "@/components/server/AppNav";
 import VideoPlayer from "./VideoPlayer";
-import { FullContent } from "@/components/layouts/OriolesLayout";
 
 export const metadata = {
-    title: 'VIP Video'
+  title: 'VIP Video'
 }
 
 export default () => {
-  return (
-    <FullContent paths={[{ name: 'VIP Video' }]} >
-      <VideoPlayer />
-    </FullContent>
-  )
+    return (
+        <div className='w-full p-4 md:p-8 flex flex-col gap-4 md:gap-6'>
+            <AppNav paths={[{ name: 'VIP Video' }]} />
+            <VideoPlayer />
+        </div>
+    )
 }
