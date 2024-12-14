a2713ee3646c559b1663da545ae6b956700180b1
sunjunzhao <zhaozhao200295@gmail.com>
2024-12-14 10:52:46 +0800

    更新自定义缓存策略

--- src/app/api/apps/route.ts
+++ src/app/api/apps/route.ts
@@ -4,11 +4,17 @@ import { getApps } from '@/model/App';
 
 export const GET = async (_: NextRequest) => {
     try {
-        return NextResponse.json({
+        const response = NextResponse.json({
             code: 200,
             data: await getApps(),
             message: '请求成功'
         });
+
+        const cacheParams = 'private, no-store';
+    response.headers.set('Cache-Control', cacheParams);
+    response.headers.set('CDN-Cache-Control', cacheParams);
+    response.headers.set('Vercel-CDN-Cache-Control', cacheParams);
+    return response;
     } catch (error) {
         return NextResponse.json(
             handleApiError(error),
